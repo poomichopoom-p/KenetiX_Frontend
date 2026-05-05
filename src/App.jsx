3bf2607d5c3716541ce0a0e413098899e610d25c
jakkrapongcwork-netizen <jakkrapongc.work@gmail.com>
2026-05-05 14:51:10 +0700

    Add massege to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div>
           
-          <h1>Get started to day.</h1>
+          <h1>Get started to day. Tae</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
