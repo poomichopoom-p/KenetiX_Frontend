083333051a37627921d3cf283c3f0dccf34bc43e
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-05 14:25:28 +0700

    New branch

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          
+          <h1>Get started to day.</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
